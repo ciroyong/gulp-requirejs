672b9181efa6438c2617e1fcef244724884a93b2
ciroyong <ciroyong@hotmail.com>
2015-05-25 20:11:35 +0800

    by ciroyong

--- index.js
+++ index.js
@@ -1,320 +1,346 @@
 module.exports = function(options) {
 	'use strict';
 
-	var requirejs, through, gutil, cache;
+	var requirejs, path, through, gutil, cache, isWindows, windowsDriveRegExp;
 
+	isWindows = process.platform === 'win32';
+	windowsDriveRegExp = /^[a-zA-Z]\:\/$/;
 	requirejs = require("requirejs");
 	through = require("through2");
 	gutil = require("gulp-util");
-	cache = {};
+	path = require("path");
+	cache = [];
 
-	requirejs.define("node/file", ["fs", "path", "prim"], function(fs, path, prim) {
-		var isWindows = process.platform === 'win32',
-			windowsDriveRegExp = /^[a-zA-Z]\:\/$/,
-			file;
+	function frontSlash(path) {
+		return path.replace(/\\/g, '/');
+	}
 
-		function frontSlash(path) {
-			return path.replace(/\\/g, '/');
+	function exists(path) {
+		if (isWindows && path.charAt(path.length - 1) === '/' &&
+			path.charAt(path.length - 2) !== ':') {
+			path = path.substring(0, path.length - 1);
 		}
 
-		function exists(path) {
-			// if (isWindows && path.charAt(path.length - 1) === '/' &&
-			// 	path.charAt(path.length - 2) !== ':') {
-			// 	path = path.substring(0, path.length - 1);
-			// }
-
-			// try {
-			// 	fs.statSync(path);
-			// 	return true;
-			// } catch (e) {
-			// 	return false;
-			// }
-			return cache.hasOwnProperty(path);
+		try {
+			fs.statSync(path);
+			return true;
+		} catch (e) {
+			return false;
 		}
+		// return cache.hasOwnProperty(path);
+	}
 
-		function mkDir(dir) {
-			// if (!exists(dir) && (!isWindows || !windowsDriveRegExp.test(dir))) {
-			// 	fs.mkdirSync(dir, 511);
-			// }
+	function mkDir(dir) {
+		if (!exists(dir) && (!isWindows || !windowsDriveRegExp.test(dir))) {
+			fs.mkdirSync(dir, 511);
 		}
+	}
 
-		function mkFullDir(dir) {
-			// var parts = dir.split('/'),
-			// 	currDir = '',
-			// 	first = true;
+	function mkFullDir(dir) {
+		var parts = dir.split('/'),
+			currDir = '',
+			first = true;
 
-			// parts.forEach(function(part) {
-			// 	//First part may be empty string if path starts with a slash.
-			// 	currDir += part + '/';
-			// 	first = false;
+		parts.forEach(function(part) {
+			//First part may be empty string if path starts with a slash.
+			currDir += part + '/';
+			first = false;
 
-			// 	if (part) {
-			// 		mkDir(currDir);
-			// 	}
-			// });
-		}
+			if (part) {
+				mkDir(currDir);
+			}
+		});
+	}
 
-		file = {
-			backSlashRegExp: /\\/g,
-			exclusionRegExp: /^\./,
-			getLineSeparator: function() {
-				return '/';
-			},
+	function start(file, encoding, callback) {
+		file.path = path.resolve(options.dir, file.relative);
+		cache[file.path] = file;
+		callback();
+	}
 
-			exists: function(fileName) {
-				return exists(fileName);
-			},
+	function end(callback) {
+		// console.log(cache);
+		// requirejs.require("node/file", function(file) {
+		// 	console.log(file.copyDir);
+		// });
+		requirejs.define("node/file", ["fs", "path", "prim"], function(fs, path, prim) {
+			return {
+				backSlashRegExp: /\\/g,
+				exclusionRegExp: /^\./,
+				getLineSeparator: function() {
+					return '/';
+				},
 
-			parent: function(fileName) {
-				var parts = fileName.split('/');
-				parts.pop();
-				return parts.join('/');
-			},
+				exists: function(fileName) {
+					console.log("[exists] fileName: %s", fileName);
+					// return exists(fileName);
+					return true;
+				},
 
-			/**
-			 * Gets the absolute file path as a string, normalized
-			 * to using front slashes for path separators.
-			 * @param {String} fileName
-			 */
-			absPath: function(fileName) {
-				return frontSlash(path.normalize(frontSlash(fs.realpathSync(fileName))));
-			},
+				parent: function(fileName) {
+					var parts = fileName.split('/');
+					parts.pop();
+					return parts.join('/');
+				},
 
-			normalize: function(fileName) {
-				return frontSlash(path.normalize(fileName));
-			},
+				/**
+				 * Gets the absolute file path as a string, normalized
+				 * to using front slashes for path separators.
+				 * @param {String} fileName
+				 */
+				absPath: function(fileName) {
+					return frontSlash(path.normalize(frontSlash(fs.realpathSync(fileName))));
+				},
 
-			isFile: function(path) {
-				return fs.statSync(path).isFile();
-			},
+				normalize: function(fileName) {
+					return frontSlash(path.normalize(fileName));
+				},
 
-			isDirectory: function(path) {
-				return fs.statSync(path).isDirectory();
-			},
+				isFile: function(path) {
+					return fs.statSync(path).isFile();
+				},
 
-			getFilteredFileList: function( /*String*/ startDir, /*RegExp*/ regExpFilters, /*boolean?*/ makeUnixPaths) {
-				//summary: Recurses startDir and finds matches to the files that match regExpFilters.include
-				//and do not match regExpFilters.exclude. Or just one regexp can be passed in for regExpFilters,
-				//and it will be treated as the "include" case.
-				//Ignores files/directories that start with a period (.) unless exclusionRegExp
-				//is set to another value.
-				var files = [],
-					topDir, regExpInclude, regExpExclude, dirFileArray,
-					i, stat, filePath, ok, dirFiles, fileName;
+				isDirectory: function(path) {
+					return fs.statSync(path).isDirectory();
+				},
 
-				topDir = startDir;
+				getFilteredFileList: function( /*String*/ startDir, /*RegExp*/ regExpFilters, /*boolean?*/ makeUnixPaths) {
+					console.log("[getFilteredFileList] startDir: %s", startDir);
+					//summary: Recurses startDir and finds matches to the files that match regExpFilters.include
+					//and do not match regExpFilters.exclude. Or just one regexp can be passed in for regExpFilters,
+					//and it will be treated as the "include" case.
+					//Ignores files/directories that start with a period (.) unless exclusionRegExp
+					//is set to another value.
+					var files = [],
+						topDir, regExpInclude, regExpExclude, dirFileArray,
+						i, stat, filePath, ok, dirFiles, fileName;
 
-				regExpInclude = regExpFilters.include || regExpFilters;
-				regExpExclude = regExpFilters.exclude || null;
+					topDir = startDir;
 
-				if (file.exists(topDir)) {
-					dirFileArray = fs.readdirSync(topDir);
-					for (i = 0; i < dirFileArray.length; i++) {
-						fileName = dirFileArray[i];
-						filePath = path.join(topDir, fileName);
-						stat = fs.statSync(filePath);
-						if (stat.isFile()) {
-							if (makeUnixPaths) {
-								//Make sure we have a JS string.
-								if (filePath.indexOf("/") === -1) {
-									filePath = frontSlash(filePath);
+					regExpInclude = regExpFilters.include || regExpFilters;
+					regExpExclude = regExpFilters.exclude || null;
+
+					if (file.exists(topDir)) {
+						dirFileArray = fs.readdirSync(topDir);
+						for (i = 0; i < dirFileArray.length; i++) {
+							fileName = dirFileArray[i];
+							filePath = path.join(topDir, fileName);
+							stat = fs.statSync(filePath);
+							if (stat.isFile()) {
+								if (makeUnixPaths) {
+									//Make sure we have a JS string.
+									if (filePath.indexOf("/") === -1) {
+										filePath = frontSlash(filePath);
+									}
 								}
-							}
 
-							ok = true;
-							if (regExpInclude) {
-								ok = filePath.match(regExpInclude);
-							}
-							if (ok && regExpExclude) {
-								ok = !filePath.match(regExpExclude);
-							}
+								ok = true;
+								if (regExpInclude) {
+									ok = filePath.match(regExpInclude);
+								}
+								if (ok && regExpExclude) {
+									ok = !filePath.match(regExpExclude);
+								}
 
-							if (ok && (!file.exclusionRegExp ||
-									!file.exclusionRegExp.test(fileName))) {
-								files.push(filePath);
+								if (ok && (!file.exclusionRegExp ||
+										!file.exclusionRegExp.test(fileName))) {
+									files.push(filePath);
+								}
+							} else if (stat.isDirectory() &&
+								(!file.exclusionRegExp || !file.exclusionRegExp.test(fileName))) {
+								dirFiles = this.getFilteredFileList(filePath, regExpFilters, makeUnixPaths);
+								files.push.apply(files, dirFiles);
 							}
-						} else if (stat.isDirectory() &&
-							(!file.exclusionRegExp || !file.exclusionRegExp.test(fileName))) {
-							dirFiles = this.getFilteredFileList(filePath, regExpFilters, makeUnixPaths);
-							files.push.apply(files, dirFiles);
 						}
 					}
-				}
 
-				return files; //Array
-			},
+					return files; //Array
+				},
 
-			copyDir: function( /*String*/ srcDir, /*String*/ destDir, /*RegExp?*/ regExpFilter, /*boolean?*/ onlyCopyNew) {
-				//summary: copies files from srcDir to destDir using the regExpFilter to determine if the
-				//file should be copied. Returns a list file name strings of the destinations that were copied.
-				regExpFilter = regExpFilter || /\w/;
-
-				//Normalize th directory names, but keep front slashes.
-				//path module on windows now returns backslashed paths.
-				srcDir = frontSlash(path.normalize(srcDir));
-				destDir = frontSlash(path.normalize(destDir));
-
-				var fileNames = file.getFilteredFileList(srcDir, regExpFilter, true),
-					copiedFiles = [],
-					i, srcFileName, destFileName;
-
-				for (i = 0; i < fileNames.length; i++) {
-					srcFileName = fileNames[i];
-					destFileName = srcFileName.replace(srcDir, destDir);
-
-					if (file.copyFile(srcFileName, destFileName, onlyCopyNew)) {
-						copiedFiles.push(destFileName);
-					}
-				}
-
-				return copiedFiles.length ? copiedFiles : null; //Array or null
-			},
-
-			copyFile: function( /*String*/ srcFileName, /*String*/ destFileName, /*boolean?*/ onlyCopyNew) {
-				//summary: copies srcFileName to destFileName. If onlyCopyNew is set, it only copies the file if
-				//srcFileName is newer than destFileName. Returns a boolean indicating if the copy occurred.
-				var parentDir;
-
-				//logger.trace("Src filename: " + srcFileName);
-				//logger.trace("Dest filename: " + destFileName);
-
-				//If onlyCopyNew is true, then compare dates and only copy if the src is newer
-				//than dest.
-				if (onlyCopyNew) {
-					if (file.exists(destFileName) && fs.statSync(destFileName).mtime.getTime() >= fs.statSync(srcFileName).mtime.getTime()) {
-						return false; //Boolean
-					}
-				}
-
-				//Make sure destination dir exists.
-				parentDir = path.dirname(destFileName);
-				if (!file.exists(parentDir)) {
-					mkFullDir(parentDir);
-				}
-
-				fs.writeFileSync(destFileName, fs.readFileSync(srcFileName, 'binary'), 'binary');
-
-				return true; //Boolean
-			},
-
-			/**
-			 * Renames a file. May fail if "to" already exists or is on another drive.
-			 */
-			renameFile: function(from, to) {
-				return fs.renameSync(from, to);
-			},
-
-			/**
-			 * Reads a *text* file.
-			 */
-			readFile: function( /*String*/ path, /*String?*/ encoding) {
-				if (encoding === 'utf-8') {
-					encoding = 'utf8';
-				}
-				if (!encoding) {
-					encoding = 'utf8';
-				}
-
-				var text = fs.readFileSync(path, encoding);
-
-				//Hmm, would not expect to get A BOM, but it seems to happen,
-				//remove it just in case.
-				if (text.indexOf('\uFEFF') === 0) {
-					text = text.substring(1, text.length);
-				}
-
-				return text;
-			},
-
-			readFileAsync: function(path, encoding) {
-				var d = prim();
-				try {
-					d.resolve(file.readFile(path, encoding));
-				} catch (e) {
-					d.reject(e);
-				}
-				return d.promise;
-			},
-
-			saveUtf8File: function( /*String*/ fileName, /*String*/ fileContents) {
-				//summary: saves a *text* file using UTF-8 encoding.
-				file.saveFile(fileName, fileContents, "utf8");
-			},
-
-			saveFile: function( /*String*/ fileName, /*String*/ fileContents, /*String?*/ encoding) {
-				//summary: saves a *text* file.
-				var parentDir;
-
-				if (encoding === 'utf-8') {
-					encoding = 'utf8';
-				}
-				if (!encoding) {
-					encoding = 'utf8';
-				}
-
-				//Make sure destination directories exist.
-				parentDir = path.dirname(fileName);
-				if (!file.exists(parentDir)) {
-					mkFullDir(parentDir);
-				}
-
-				fs.writeFileSync(fileName, fileContents, encoding);
-			},
-
-			deleteFile: function( /*String*/ fileName) {
-				//summary: deletes a file or directory if it exists.
-				var files, i, stat;
-				if (file.exists(fileName)) {
-					stat = fs.lstatSync(fileName);
-					if (stat.isDirectory()) {
-						files = fs.readdirSync(fileName);
-						for (i = 0; i < files.length; i++) {
-							this.deleteFile(path.join(fileName, files[i]));
+				copyDir: function( /*String*/ srcDir, /*String*/ destDir, /*RegExp?*/ regExpFilter, /*boolean?*/ onlyCopyNew) {
+					console.log("[copy dir], from: %s, to: %s", srcDir, destDir);
+					var copiedFiles = [];
+					for(var name in cache) {
+						if(cache[name].isBuffer()){
+							copiedFiles.push(cache[name].path);
 						}
-						fs.rmdirSync(fileName);
-					} else {
-						fs.unlinkSync(fileName);
 					}
-				}
-			},
+					//summary: copies files from srcDir to destDir using the regExpFilter to determine if the
+					//file should be copied. Returns a list file name strings of the destinations that were copied.
+					// regExpFilter = regExpFilter || /\w/;
 
+					// //Normalize th directory names, but keep front slashes.
+					// //path module on windows now returns backslashed paths.
+					// srcDir = frontSlash(path.normalize(srcDir));
+					// destDir = frontSlash(path.normalize(destDir));
 
-			/**
-			 * Deletes any empty directories under the given directory.
-			 */
-			deleteEmptyDirs: function(startDir) {
-				var dirFileArray, i, fileName, filePath, stat;
+					// var fileNames = file.getFilteredFileList(srcDir, regExpFilter, true),
+					// 	copiedFiles = [],
+					// 	i, srcFileName, destFileName;
 
-				if (file.exists(startDir)) {
-					dirFileArray = fs.readdirSync(startDir);
-					for (i = 0; i < dirFileArray.length; i++) {
-						fileName = dirFileArray[i];
-						filePath = path.join(startDir, fileName);
-						stat = fs.lstatSync(filePath);
+					// for (i = 0; i < fileNames.length; i++) {
+					// 	srcFileName = fileNames[i];
+					// 	destFileName = srcFileName.replace(srcDir, destDir);
+
+					// 	if (file.copyFile(srcFileName, destFileName, onlyCopyNew)) {
+					// 		copiedFiles.push(destFileName);
+					// 	}
+					// }
+
+					// return copiedFiles.length ? copiedFiles : null; //Array or null
+					console.log(copiedFiles);
+					return copiedFiles;
+				},
+
+				copyFile: function( /*String*/ srcFileName, /*String*/ destFileName, /*boolean?*/ onlyCopyNew) {
+					console.log("[copy file]srcFileName: %s, destFileName: %s", srcFileName, destFileName);
+					return true;
+					//summary: copies srcFileName to destFileName. If onlyCopyNew is set, it only copies the file if
+					//srcFileName is newer than destFileName. Returns a boolean indicating if the copy occurred.
+					// var parentDir;
+
+					// //logger.trace("Src filename: " + srcFileName);
+					// //logger.trace("Dest filename: " + destFileName);
+
+					// //If onlyCopyNew is true, then compare dates and only copy if the src is newer
+					// //than dest.
+					// if (onlyCopyNew) {
+					// 	if (file.exists(destFileName) && fs.statSync(destFileName).mtime.getTime() >= fs.statSync(srcFileName).mtime.getTime()) {
+					// 		return false; //Boolean
+					// 	}
+					// }
+
+					// //Make sure destination dir exists.
+					// parentDir = path.dirname(destFileName);
+					// if (!file.exists(parentDir)) {
+					// 	mkFullDir(parentDir);
+					// }
+
+					// fs.writeFileSync(destFileName, fs.readFileSync(srcFileName, 'binary'), 'binary');
+
+					// return true; //Boolean
+				},
+
+				/**
+				 * Renames a file. May fail if "to" already exists or is on another drive.
+				 */
+				renameFile: function(from, to) {
+					console.log("[renameFile] form: %s, to: %s", from, to);
+					return fs.renameSync(from, to);
+				},
+
+				/**
+				 * Reads a *text* file.
+				 */
+				readFile: function( /*String*/ path, /*String?*/ encoding) {
+					console.log("[read file]path: %s, encoding: %s", path, encoding);
+					// if (encoding === 'utf-8') {
+					// 	encoding = 'utf8';
+					// }
+					// if (!encoding) {
+					// 	encoding = 'utf8';
+					// }
+
+					// var text = fs.readFileSync(path, encoding);
+
+					// //Hmm, would not expect to get A BOM, but it seems to happen,
+					// //remove it just in case.
+					// if (text.indexOf('\uFEFF') === 0) {
+					// 	text = text.substring(1, text.length);
+					// }
+
+					// return text;
+					return "";
+				},
+
+				readFileAsync: function(path, encoding) {
+					console.log("[readFileAsync]path: %s, encodeing: %s", path, encodeing);
+					// var d = prim();
+					// try {
+					// 	d.resolve(file.readFile(path, encoding));
+					// } catch (e) {
+					// 	d.reject(e);
+					// }
+					// return d.promise;
+				},
+
+				saveUtf8File: function( /*String*/ fileName, /*String*/ fileContents) {
+					console.log("[save utf-8 file]");
+					//summary: saves a *text* file using UTF-8 encoding.
+					file.saveFile(fileName, fileContents, "utf8");
+				},
+
+				saveFile: function( /*String*/ fileName, /*String*/ fileContents, /*String?*/ encoding) {
+					console.log("[save file]");
+					//summary: saves a *text* file.
+					var parentDir;
+
+					if (encoding === 'utf-8') {
+						encoding = 'utf8';
+					}
+					if (!encoding) {
+						encoding = 'utf8';
+					}
+
+					//Make sure destination directories exist.
+					parentDir = path.dirname(fileName);
+					if (!file.exists(parentDir)) {
+						mkFullDir(parentDir);
+					}
+
+					fs.writeFileSync(fileName, fileContents, encoding);
+				},
+
+				deleteFile: function( /*String*/ fileName) {
+					console.log("[deleteFile] filename: %s", fileName);
+					//summary: deletes a file or directory if it exists.
+					var files, i, stat;
+					if (file.exists(fileName)) {
+						stat = fs.lstatSync(fileName);
 						if (stat.isDirectory()) {
-							file.deleteEmptyDirs(filePath);
+							files = fs.readdirSync(fileName);
+							for (i = 0; i < files.length; i++) {
+								this.deleteFile(path.join(fileName, files[i]));
+							}
+							fs.rmdirSync(fileName);
+						} else {
+							fs.unlinkSync(fileName);
 						}
 					}
+				},
 
-					//If directory is now empty, remove it.
-					if (fs.readdirSync(startDir).length === 0) {
-						file.deleteFile(startDir);
+
+				/**
+				 * Deletes any empty directories under the given directory.
+				 */
+				deleteEmptyDirs: function(startDir) {
+					console.log("[deleteEmptyDirs] startDir: %s", startDir);
+					var dirFileArray, i, fileName, filePath, stat;
+
+					if (file.exists(startDir)) {
+						dirFileArray = fs.readdirSync(startDir);
+						for (i = 0; i < dirFileArray.length; i++) {
+							fileName = dirFileArray[i];
+							filePath = path.join(startDir, fileName);
+							stat = fs.lstatSync(filePath);
+							if (stat.isDirectory()) {
+								file.deleteEmptyDirs(filePath);
+							}
+						}
+
+						//If directory is now empty, remove it.
+						if (fs.readdirSync(startDir).length === 0) {
+							file.deleteFile(startDir);
+						}
 					}
 				}
 			}
-		};
+		});
 
-		return file;
-
-	});
-
-	return through.obj(function(file, encoding, callback) {
-		cache[file.path] = file;
-		callback();
-	}, function(callback) {
-		console.log(cache);
 		requirejs.optimize(options, function() {
 			callback()
 		}, function() {})
-	});
+	}
+
+	return through.obj(start, end);
 };
